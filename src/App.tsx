32ad11208415a07e1bfff2aec90d8ad409651c16
me-issyaiful <me.issyaiful@gmail.com>
2025-06-19 17:31:19 +0700

    Forgot Password Page Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import PropertyListingPage from './pages/PropertyListingPage';
 import PropertyDetailPage from './pages/PropertyDetailPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path="/properti/:id" element={<PropertyDetailPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
           {/* Add more routes as needed */}
         </Routes>
       </Router>
